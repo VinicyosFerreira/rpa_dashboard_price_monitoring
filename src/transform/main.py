80310755f542f90b57bbdc4d3e9fc36dbd75fc26
Vinicyos Ferreira <vinifr987@gmail.com>
2026-02-12 19:22:11 -0300

    refactor: add class Transform to standardize reuse new instances

--- src/transform/main.py
+++ src/transform/main.py
@@ -3,64 +3,67 @@ import pandas as pd
 import datetime as dt 
 import os
 
- # read data json file
-df = pd.read_json('/home/vinicyos/python_projetos/monitoramento_preco/data/products.jsonl', lines=True)
+class Transform: 
+     # read data json file
+    def __init__(self):
+        self.__df = pd.read_json('/home/vinicyos/python_projetos/monitoramento_preco/data/products.jsonl', lines=True)
+    
+    def execute(self):
+        # transformations here, astype to convert data types
+        self.__df['currentPrice'] = self.__df['currentPrice'].astype(float)
+        self.__df['oldPrice'] = self.__df['oldPrice'].astype(float)
 
-# transformations here, astype to convert data types
-df['currentPrice'] = df['currentPrice'].astype(float)
-df['oldPrice'] = df['oldPrice'].astype(float)
+        # separate rating into two columns: rating_score and rating_count_sales
+        newColumns = self.__df['rating'].str.split("de 5 estrelas." , n=1, expand=True)
+        self.__df['rating_score'] = newColumns[0]
+        self.__df['rating_count_sales'] = newColumns[1]
 
-# separate rating into two columns: rating_score and rating_count_sales
-newColumns = df['rating'].str.split("de 5 estrelas." , n=1, expand=True)
-df['rating_score'] = newColumns[0]
-df['rating_count_sales'] = newColumns[1]
+        # clean rating_score word Classificação and type cast to float
+        self.__df['rating_score'] = self.__df['rating_score'].str.replace('Classificação', '')
 
-# clean rating_score word Classificação and type cast to float
-df['rating_score'] = df['rating_score'].str.replace('Classificação', '')
+        # clean rating_count_sales final point
+        self.__df['rating_count_sales'] = self.__df['rating_count_sales'].str.replace('.' , '')
 
-# clean rating_count_sales final point
-df['rating_count_sales'] = df['rating_count_sales'].str.replace('.' , '')
+        # Store with null change by default value "Loja não informada"
+        self.__df['store'] = self.__df['store'].fillna('Loja não informada')
 
-# Store with null change by default value "Loja não informada"
-df['store'] = df['store'].fillna('Loja não informada')
+        # oldPrice with null change by default currentPrice value
+        self.__df['oldPrice'] = self.__df['oldPrice'].fillna(self.__df['currentPrice'])
 
-# oldPrice with null change by default currentPrice value
-df['oldPrice'] = df['oldPrice'].fillna(df['currentPrice'])
+        # rating count sales, rating score with null change by default value N/A
+        self.__df['rating_count_sales'] = self.__df['rating_count_sales'].fillna('Não informado')
+        self.__df['rating_score'] = self.__df['rating_score'].fillna('Não informado')
 
-# rating count sales, rating score with null change by default value N/A
-df['rating_count_sales'] = df['rating_count_sales'].fillna('Não informado')
-df['rating_score'] = df['rating_score'].fillna('Não informado')
+        contains_not_informed = self.__df['rating_count_sales'].str.contains('Não informado').any()
+        if contains_not_informed: 
+            # remove rows with rating_count_sales == 'Não informado'
+            self.__df = self.__df.drop(self.__df[self.__df['rating_count_sales'] == 'Não informado'].index)
+            self.__df = self.__df.reset_index(drop=True)
 
-contains_not_informed = df['rating_count_sales'].str.contains('Não informado').any()
-if contains_not_informed: 
-    # remove rows with rating_count_sales == 'Não informado'
-    df = df.drop(df[df['rating_count_sales'] == 'Não informado'].index)
-    df = df.reset_index(drop=True)
+        self.__df['rating_score'] = self.__df['rating_score'].astype(float)
+                    
+        # removing point price
+        self.__df['currentPrice'] = self.__df['currentPrice'].astype(str).str.replace('.' , '')
+        self.__df['oldPrice'] = self.__df['oldPrice'].astype(str).str.replace('.' , '')
+        # cast to float
+        self.__df['currentPrice'] = pd.to_numeric(self.__df['currentPrice'], errors='coerce')
+        self.__df['oldPrice'] = pd.to_numeric(self.__df['oldPrice'], errors='coerce')
 
-df['rating_score'] = df['rating_score'].astype(float)
-            
-# removing point price
-df['currentPrice'] = df['currentPrice'].astype(str).str.replace('.' , '')
-df['oldPrice'] = df['oldPrice'].astype(str).str.replace('.' , '')
-# cast to float
-df['currentPrice'] = pd.to_numeric(df['currentPrice'], errors='coerce')
-df['oldPrice'] = pd.to_numeric(df['oldPrice'], errors='coerce')
+        print(self.__df.info())
 
-print(df.info())
+        # drop rating column
+        self.__df = self.__df.drop(columns=['rating'])
 
-# drop rating column
-df = df.drop(columns=['rating'])
+        # create colum date with analysis 
+        self.__df['date'] = dt.datetime.now().strftime('%d/%m/%Y')
 
-# create colum date with analysis 
-df['date'] = dt.datetime.now().strftime('%d/%m/%Y')
-
-file_path = '../../data/products.csv'
-if(os.path.isfile(file_path)): 
-    print("Arquivo existente")
-    df.to_csv('/home/vinicyos/python_projetos/monitoramento_preco/data/products.csv', index=False, mode='a', header=False)
-else: 
-    print("Arquivo nao existente")
-    df.to_csv('/home/vinicyos/python_projetos/monitoramento_preco/data/products.csv', index=False)
+        file_path = '../../data/products.csv'
+        if(os.path.isfile(file_path)): 
+            print("Arquivo existente")
+            self.__df.to_csv('/home/vinicyos/python_projetos/monitoramento_preco/data/products.csv', index=False, mode='a', header=False)
+        else: 
+            print("Arquivo nao existente")
+            self.__df.to_csv('/home/vinicyos/python_projetos/monitoramento_preco/data/products.csv', index=False)
 
 
 
